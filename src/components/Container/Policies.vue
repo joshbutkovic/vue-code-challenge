314ed916facdb0faafaf93aa809cfbba9f412b47
joshbutkovic <joshbutkovic@gmail.com>
2020-04-16 16:31:59 -0700

    updating content

--- src/components/Container/Policies.vue
+++ src/components/Container/Policies.vue
@@ -12,42 +12,7 @@
                             condimentum lacinia quis vel eros. Molestie nunc non
                             blandit massa enim nec. Sit amet aliquam id diam.
                             Semper risus in hendrerit gravida rutrum quisque
-                            non. Elementum eu facilisis sed odio morbi. Nec
-                            feugiat in fermentum posuere. Mi ipsum faucibus
-                            vitae aliquet nec ullamcorper sit amet risus. Ut
-                            consequat semper viverra nam libero. Nunc congue
-                            nisi vitae suscipit tellus mauris. Massa tincidunt
-                            nunc pulvinar sapien et ligula ullamcorper
-                            malesuada. Ultricies integer quis auctor elit sed
-                            vulputate mi sit amet. Turpis egestas pretium aenean
-                            pharetra magna ac placerat vestibulum. Cursus risus
-                            at ultrices mi. Neque viverra justo nec ultrices dui
-                            sapien eget mi. Iaculis eu non diam phasellus
-                            vestibulum.
-                        </p>
-                        <br />
-                        <p>
-                            Urna nunc id cursus metus aliquam eleifend. Purus
-                            sit amet volutpat consequat. Magna etiam tempor orci
-                            eu lobortis elementum. Odio eu feugiat pretium nibh
-                            ipsum consequat nisl vel. Turpis egestas integer
-                            eget aliquet nibh praesent tristique magna sit.
-                            Blandit turpis cursus in hac habitasse platea
-                            dictumst. Tortor posuere ac ut consequat semper.
-                            Vestibulum sed arcu non odio euismod lacinia at quis
-                            risus. Egestas purus viverra accumsan in nisl nisi.
-                            Orci porta non pulvinar neque laoreet suspendisse
-                            interdum. Maecenas accumsan lacus vel facilisis.
-                            Sapien et ligula ullamcorper malesuada proin libero
-                            nunc consequat. At varius vel pharetra vel turpis
-                            nunc eget. Sagittis vitae et leo duis ut diam quam.
-                            Pellentesque sit amet porttitor eget dolor morbi
-                            non. Risus sed vulputate odio ut enim blandit
-                            volutpat. Pulvinar mattis nunc sed blandit libero
-                            volutpat sed cras ornare. Sed viverra tellus in hac
-                            habitasse platea. Placerat orci nulla pellentesque
-                            dignissim enim sit amet venenatis urna. Interdum
-                            velit laoreet id donec ultrices tincidunt arcu.
+                            non. Elementum eu facilisis sed odio morbi.
                         </p>
                     </div>
                 </div>
