0899e295509fca1892ecd391156a89754f98c7b7
joshbutkovic <joshbutkovic@gmail.com>
2020-04-16 14:33:56 -0700

    adding lorem ipsum

--- src/components/Container/Safety.vue
+++ src/components/Container/Safety.vue
@@ -5,7 +5,50 @@
                 <div class="hero-body">
                     <div class="container">
                         <p class="title">Safety</p>
-                        <p class="subtitle">Coming Soon</p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Sit amet massa vitae tortor
+                            condimentum lacinia quis vel eros. Molestie nunc non
+                            blandit massa enim nec. Sit amet aliquam id diam.
+                            Semper risus in hendrerit gravida rutrum quisque
+                            non. Elementum eu facilisis sed odio morbi. Nec
+                            feugiat in fermentum posuere. Mi ipsum faucibus
+                            vitae aliquet nec ullamcorper sit amet risus. Ut
+                            consequat semper viverra nam libero. Nunc congue
+                            nisi vitae suscipit tellus mauris. Massa tincidunt
+                            nunc pulvinar sapien et ligula ullamcorper
+                            malesuada. Ultricies integer quis auctor elit sed
+                            vulputate mi sit amet. Turpis egestas pretium aenean
+                            pharetra magna ac placerat vestibulum. Cursus risus
+                            at ultrices mi. Neque viverra justo nec ultrices dui
+                            sapien eget mi. Iaculis eu non diam phasellus
+                            vestibulum.
+                        </p>
+                        <br />
+                        <p>
+                            Urna nunc id cursus metus aliquam eleifend. Purus
+                            sit amet volutpat consequat. Magna etiam tempor orci
+                            eu lobortis elementum. Odio eu feugiat pretium nibh
+                            ipsum consequat nisl vel. Turpis egestas integer
+                            eget aliquet nibh praesent tristique magna sit.
+                            Blandit turpis cursus in hac habitasse platea
+                            dictumst. Tortor posuere ac ut consequat semper.
+                            Vestibulum sed arcu non odio euismod lacinia at quis
+                            risus. Egestas purus viverra accumsan in nisl nisi.
+                            Orci porta non pulvinar neque laoreet suspendisse
+                            interdum. Maecenas accumsan lacus vel facilisis.
+                            Sapien et ligula ullamcorper malesuada proin libero
+                            nunc consequat. At varius vel pharetra vel turpis
+                            nunc eget. Sagittis vitae et leo duis ut diam quam.
+                            Pellentesque sit amet porttitor eget dolor morbi
+                            non. Risus sed vulputate odio ut enim blandit
+                            volutpat. Pulvinar mattis nunc sed blandit libero
+                            volutpat sed cras ornare. Sed viverra tellus in hac
+                            habitasse platea. Placerat orci nulla pellentesque
+                            dignissim enim sit amet venenatis urna. Interdum
+                            velit laoreet id donec ultrices tincidunt arcu.
+                        </p>
                     </div>
                 </div>
             </section>
@@ -18,5 +61,3 @@ export default {
     name: 'Welcome',
 };
 </script>
-
-
